6817a7f36e618f9b514abe156a1d16bda3bb5bfd
Serkan14520 <enchevserkan@gmail.com>
2026-08-20 17:13:52 +0200

    Add input validation and error handling

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@
         static void Main(string[] args)
         {
             int keuze = 0;
+            bool geldigeInvoer = false;
 
             Bankrekening bankrekening = new Bankrekening();
 
@@ -15,47 +16,97 @@
 
             Console.WriteLine("Wat is je beginsaldo?");
 
-            bankrekening.saldo = int.Parse(Console.ReadLine());
+            while (!geldigeInvoer)
+            {
+                try
 
-            
-
+                {
+                    bankrekening.saldo = int.Parse(Console.ReadLine());
+                    geldigeInvoer = true;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Ongeldige invoer. Voer een geldig getal in.");
+                    continue;
+                }
+            }
 
             while (keuze != 4)
             {
-            Console.WriteLine("Kies een optie:");
+              
 
-            Console.WriteLine("1. Saldo bekijken");
+                Console.WriteLine("Kies een optie:");
 
-            Console.WriteLine("2. Geld storten");
+                    Console.WriteLine("1. Saldo bekijken");
 
-            Console.WriteLine("3. Geld opnemen");
+                    Console.WriteLine("2. Geld storten");
 
-            Console.WriteLine("4. Stoppen");
+                    Console.WriteLine("3. Geld opnemen");
 
+                    Console.WriteLine("4. Stoppen");
 
-             keuze = int.Parse(Console.ReadLine());
+                try
+                {
+                    keuze = int.Parse(Console.ReadLine());
+
+                }
+                catch (FormatException)
+
+                {
+                    Console.WriteLine("Ongeldige invoer. Voer een geldig getal in.");
+                    continue;
+                }
+      
 
                 switch (keuze)
                 {
                     case 1:
                         Console.WriteLine("Hallo, " + bankrekening.naam + "! Je saldo is: " + bankrekening.saldo); ;
+                        
                         break;
                     case 2:
                         Console.WriteLine("Hoeveel wil je storten op je saldo?");
-                        int storting = int.Parse(Console.ReadLine());
-                        bankrekening.Storten(storting);
-                        break;
+
+                        
+                        try
+                        {
+                            int storting = int.Parse(Console.ReadLine());
+                            bankrekening.Storten(storting);
+                            break;
+                        }
+
+                        catch (FormatException)
+                        {
+                            Console.WriteLine("Ongeldige invoer. Voer een geldig getal in.");
+                            continue;
+                        }
+                        
                     case 3:
                         Console.WriteLine("Hoeveel wil je opnemen?");
-                        int opname = int.Parse(Console.ReadLine());
-                        bankrekening.Opnemen(opname);
+
+
+                        try
+                        {
+                            int opname = int.Parse(Console.ReadLine());
+                            bankrekening.Opnemen(opname);
+                            break;
+
+                        }
+
+                        catch (FormatException)
+                        {
+                            Console.WriteLine("Ongeldige invoer. Voer een geldig getal in.");
+                            continue;
+                        }
+
+                    default:
+                        Console.WriteLine("Ongeldige keuze, probeer het opnieuw.");
                         break;
+
                     case 4:
                         Console.WriteLine("Bedankt voor het gebruiken van de Bankrekening Simulator!");
                         return;
-            }   }
-
-           
+            }   }  
         }
     }
       
